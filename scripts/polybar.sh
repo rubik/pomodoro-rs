76bcfef07897c68fbcf48950b17e8d4a2509f442
Michele Lacchia <michelelacchia@gmail.com>
2020-10-07 09:48:36 +0200

    chore: update Polybar script to handle connection error

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -4,6 +4,9 @@ icon="⭕"
 
 read -a state <<< "$(pomoctl state)"
 case "${state[0]}" in
+    connection-error)
+        state_color="#FFF"
+        ;;
     stopped)
         state_color="#FFF"
         ;;
@@ -11,10 +14,11 @@ case "${state[0]}" in
         state_color="#F1C232"
         ;;
     short-break)
-        state_color="#32F29F"
+        state_color="#2AEA2A"
         ;;
     long-break)
         state_color="#2AEA2A"
+        icon="⬤ "
         ;;
 esac
 echo "%{T4}%{F$state_color}$icon%{F-}%{T-} ${state[1]}"
